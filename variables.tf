c8f14ea7f31d481fbca0fb400813e534ce299279
ashok <pasupuletiaashokkumar143@gmail.com>
2021-10-01 07:32:58 -0700

    adding terraform to github

--- variables.tf
+++ variables.tf
@@ -1,13 +1,40 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+    }
 }
-
-variable "ami_id" {
-  type = "map"
-
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  type = list
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+variable "environment" { default = "dev" }
+variable "instance_type" {
+  type = map
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
-  }
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
 }
+
